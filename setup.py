dc4d59e450a871890cc7202d05d23a5698058847
Lara Palmer (MDBA) <lp10@prod.local>
2023-01-04 17:08:48 +1100

    updated to latest ipython

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     install_requires=[
-        "ipython==8.3.0",
+        "ipython==8.8.0",
         "ipywidgets==7.7.0",
         "pandas==1.4.2",
         "requests==2.25.1",
