4150306d8e640d6d49b9bdf005daeb0fec4ce63c
pedrojunqueira <pedrocj@gmail.com>
2023-09-06 16:47:46 +0930

    downgrading pandas to 2.0.3 to allow 3.8 compatibility

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "ipython==8.8.0",
         "ipywidgets==7.7.0",
-        "pandas==2.1.0",
+        "pandas==2.0.3",
         "requests==2.25.1",
         "tqdm==4.64.0",
         "mdba-gauge-getter==0.5.0",
