af38172590d8f12f126a64e7ec42481bc81d79dd
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-11-22 11:11:56 +1100

    netcdf4 dependency

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(
         "tqdm==4.64.0",
         "mdba-gauge-getter==0.5.1",
         "cachetools==5.2.0",
-        "xarray==2023.10.1"
+        "xarray==2023.10.1",
+        "netCDF4==1.6.4"
     ],
     package_data={'': ["model_metadata/*.csv", "parameter_metadata/*.csv","parameter_metadata/*.json"]},
 )
