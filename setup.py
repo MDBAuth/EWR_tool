ec7e936fd833d2c04a98c1b529272aae31dd3dcf
ElishaMDBA <elisha.freedman@mdba.gov.au>
2024-03-22 03:33:07 +0000

    Update version number in setup.py and README.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py_ewr",
-    version="2.1.0",
+    version="2.1.1",
     author="Martin Job",
     author_email="Martin.Job@mdba.gov.au",
     description="Environmental Water Requirement calculator",
