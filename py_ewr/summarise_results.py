cb58f89b42425cb5fde6e0654f3ba7f2625bbd23
BenBMDBA <46876990+BenBMDBA@users.noreply.github.com>
2024-05-01 11:48:42 +1000

    Update summarise_results.py

--- py_ewr/summarise_results.py
+++ py_ewr/summarise_results.py
@@ -117,6 +117,7 @@ def process_df(scenario:str, gauge:str, pu:str, pu_df: pd.DataFrame)-> pd.DataFr
     """
     ewrs = get_ewrs(pu_df)
     returned_dfs = []
+    log.error(ewrs)
     for ewr in ewrs:
         columns_ewr = get_ewr_columns(ewr, pu_df.columns.to_list())
         ewr_df = pu_df[columns_ewr]
