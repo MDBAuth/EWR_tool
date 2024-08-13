c13511a6e60500d30fb2f18668ab636a7651495d
MartinMDBA <60993472+MartinMDBA@users.noreply.github.com>
2024-08-13 18:16:24 +1000

    Update setup.py with new version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py_ewr",
-    version="2.2.2",
+    version="2.2.4",
     author="Martin Job",
     author_email="Martin.Job@mdba.gov.au",
     description="Environmental Water Requirement calculator",
