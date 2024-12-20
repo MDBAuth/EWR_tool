0005d448564ac4472272d4802ec6d43ac75d7ba3
ZofiaMDBA <zofia.francis@mdba.gov.au>
2024-12-20 06:43:56 +0000

    started but didn't get very far

---
+++ notes.md
@@ -0,0 +1,1 @@
+Just a little file with notes about any questions or thoughts I have
---
+++ py_ewr/filter.py
@@ -0,0 +1,22 @@
+import json
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import os
+import logging
+
+
+
+def get_relevant_EWRs(EWR_df, 
+                      relevance_df, 
+                      EWR_join = ['LTWPShortName','Gauge', 'Code'], 
+                      relevance_join = ['LTWPShortName','gauge','ewr_code']):
+    '''Takes in the EWR dataframe, and a data frame with a column representing
+    if an EWR is relevant to a project.
+
+    Args:
+        EWR_df
+        todo zofia finish this 
+    '''
+
+    merged_df = pd.merge(EWR_df,)
