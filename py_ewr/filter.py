75b6e15f529aea0953e6ba2921aadc7e4876e78f
ZofiaMDBA <zofia.francis@mdba.gov.au>
2025-01-06 00:02:51 +0000

    Updated Documentation for function

--- py_ewr/filter.py
+++ py_ewr/filter.py
@@ -15,8 +15,23 @@ def get_relevant_EWRs(EWR_df,
     if an EWR is relevant to a project.
 
     Args:
-        EWR_df
-        todo zofia finish this 
+        EWR_df (pd.DataFrame): Dataframe of EWRs
+        relevance_df (pd.DataFrame): Dataframe with identifying which EWRs are relevant
+        EWR_join (list): List of column names to join on for the EWR_df (foreign key)
+        relevance_join (list): List of column names to join on, corresponding to the EWR_df (foreign key)
+
+    Results:
+        EWRs (pd.DataFrame): Dataframe containing only those EWRs that are relevant
     '''
 
-    merged_df = pd.merge(EWR_df,)
+    merged_df = pd.merge(EWR_df,relevance_df)
+
+help()
+
+# if __name__ == "__main__":
+#     '''
+#     Still working out how python works, but this should allow us to run the function?
+#     '''
+#     print("still working on the get_relevant_EWRs function")
+
+    # get_relevant_EWRs() # todo, add all the parameters
