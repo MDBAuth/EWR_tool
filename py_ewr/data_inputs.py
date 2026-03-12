1516b90437152c0fa17667bd6f7976b91382d004
ElishaMDBA <161266053+ElishaMDBA@users.noreply.github.com>
2026-03-12 13:58:33 +1100

    fixed EnvObj to EcoObj for get EWR table in data_inputs.py

--- py_ewr/data_inputs.py
+++ py_ewr/data_inputs.py
@@ -575,7 +575,7 @@ def get_obj_mapping(
     
     # Split 'EnvObj' by '+' and explode to long format
     longform_ewr = df_sub.assign(
-        EnvObj=df_sub['EcoObj'].str.split('+')
+        EcoObj=df_sub['EcoObj'].str.split('+')
     ).explode('EnvObj').drop_duplicates()
 
     merged_df = longform_ewr.merge(
